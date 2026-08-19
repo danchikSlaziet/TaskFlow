7d131ea66ec54eea08adb68aa5fc92c8a8211bc3
Danis Tazhitdinov <tashitdinov1999@mail.ru>
2026-08-19 13:52:00 +0500

    chore: setup docker, standalone build, and ci/cd deploy pipeline

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone", // только необход.зависимости в сборку
 };
 
 export default nextConfig;
